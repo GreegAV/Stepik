eb24eaed65a47c79efb84c1897aed011c5f5317d
GreegAV <greegav@gmail.com>
2017-06-17 00:31:03 +0300

    doubleExpression

---
+++ src/Stepik/StepikDoubleExpression.java
@@ -0,0 +1,22 @@
+package Stepik;
+
+/**
+ Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+ Допустимая погрешность – 0.0001 (1E-4)
+
+ Можно использовать класс Math и его методы. Класс Math доступен всегда, импортировать его не надо.
+
+ В качестве примера написано заведомо неправильное выражение. Исправьте его.
+ Sample Input:
+ 0.1 0.2 0.3
+ Sample Output:
+ true
+ */
+public class StepikDoubleExpression {
+    public static void main(String[] args) {
+        System.out.println(doubleExpression(0.1, 0.2, 0.3));
+    }
+    public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs((a + b) - c) < 1E-4;
+    }
+}
