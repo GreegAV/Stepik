06654015300af53a796fd9fc5e4fee353530bbe8
GreegAV <greegav@gmail.com>
2017-06-16 22:42:57 +0300

    Initial commit

---
+++ src/Stepik.java
@@ -0,0 +1,10 @@
+/**
+ * Created by avg-m on 16/06/2017.
+ */
+public class Stepik {
+
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        return a & b & c & d;
+    }
+
+}
