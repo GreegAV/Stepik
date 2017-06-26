e527498f18b5e129d8ac3bc8ec532e33224af92f
GreegAV <greegav@gmail.com>
2017-06-26 21:40:58 +0300

    Copypaste&compilate

--- src/Stepik/MailerPackage/FilePathes.java
+++ src/Stepik/MailerPackage/FilePathes.java
@@ -8,18 +8,22 @@ import java.io.IOException;
  */
 public class FilePathes {
     public static void main(String[] args) throws IOException {
+
         File file = new File(".\\a\\b\\..\\b\\c\\.\\file.txt");
         System.out.println(".\\a\\b\\..\\b\\c\\.\\file.txt");
         System.out.println(file.getCanonicalPath());
         System.out.println("____________________________________________");
+
         File file1 = new File("a\\b\\..\\file.txt");
         System.out.println("a\\b\\..\\file.txt");
         System.out.println(file1.getCanonicalPath());
         System.out.println("____________________________________________");
+
         File file2 = new File("a\\..\\b\\c\\file.txt");
         System.out.println("a\\..\\b\\c\\file.txt");
         System.out.println(file2.getCanonicalPath());
         System.out.println("____________________________________________");
+
         File file3 = new File("a\\b\\c\\file.txt");
         System.out.println("a\\b\\c\\file.txt");
         System.out.println(file3.getCanonicalPath());
